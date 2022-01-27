1700bc3b7ada1bbc0d44798cd59b714ddf2fcc53
Daniel Ribuffo <dribuffo@gmail.com>
2022-01-26 17:07:30 -0800

    had to rebuild all 3 files

--- apps.js
+++ apps.js
@@ -2,80 +2,78 @@
         ARROW BUTTONS
 */
 function aboutMeArrow() {
-    $("#prof_am").show();
-    $("#prof_s").hide();
-    $("#prof_p").hide();
+  $(".about_me_container").show();
+  $(".skill_list").hide();
+  $(".slideshow-container").hide();
 }
 
 function skillsArrow() {
-    $("#prof_am").hide();
-    $("#prof_s").show();
-    $("#prof_p").hide();
+  $(".about_me_container").hide();
+  $(".skill_list").show();
+  $(".slideshow-container").hide();
 }
 
 function projectsArrow() {
-    $("#prof_am").hide();
-    $("#prof_s").hide();
-    $("#prof_p").show();
+  $(".about_me_container").hide();
+  $(".skill_list").hide();
+  $(".slideshow-container").show();
 }
-
 
 //----------------------------------------------------------------------
 /*
-        SLIDESHOW CONTAINER
+      SLIDESHOW CONTAINER
 */
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
 }
-
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
 
 //----------------------------------------------------------------------
 /*
-        Style Swap Slider
+      Style Swap Slider
 */
 const toggleSwitch =
-	document.querySelector('.theme-slider input[type="checkbox"]');
+document.querySelector('.theme-slider input[type="checkbox"]');
 
 /* Function to change theme */
 function switchTheme(e) {
 
-	/* Once checkbox is checked default theme change to dark */
-	if (e.target.checked) {
-		document.documentElement.setAttribute('theme', 'gamer');
-        $('slide_text').html("Swap to Professional mode.")
-	}
+/* Once checkbox is checked default theme change to dark */
+if (e.target.checked) {
+  document.documentElement.setAttribute('theme', 'gamer');
+      $('.slider_bar_text').html("Swap to Professional mode.")
+}
 
-	/* While page in dark mode and checkbox is
-	checked then theme back to change light*/
-	else {
-		document.documentElement.setAttribute('theme', 'professional');
-        $('slide_text').html("Swap to Gamer mode.")
-	}
+/* While page in dark mode and checkbox is
+checked then theme back to change light*/
+else {
+  document.documentElement.setAttribute('theme', 'professional');
+      $('.slider_bar_text').html("Swap to Gamer mode.")
+}
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
